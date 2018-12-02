a8d3e491d8f7d80370e4df9a9618d910c36e2b0d
Nadav Taragan Tarago <36599585+NadavTarago@users.noreply.github.com>
2018-12-02 23:19:30 +0200

    Issue #3 fixed
    To learn more, watch my answer (to myself) here: https://stackoverflow.com/questions/44091191/how-to-push-array-into-another-array-as-one-element
    
    Also, as .csv format require to delete whitespace before and after the relevant String, I used str.trim() function, that clear the white-spaces from the beginning and the end of the string.

--- Main.js
+++ Main.js
@@ -1,23 +1,23 @@
 /**
 	Auto-Add Conections into your linkedin account.
-	instructions: 
-	1. Open chrome, get in: https://www.linkedin.com/mynetwork/  	. Connect to your linkedin first. 
+	instructions:
+	1. Open chrome, get in: https://www.linkedin.com/mynetwork/  	. Connect to your linkedin first.
 	2. Make sure the scroll-down bar is appear (means, DO NOT open the chrome window all over your screen! scroll up-down bar should be appear and active! (do not touch scroll it down).
-	3. Open console (f12), and copy paste this script there and press enter. 
+	3. Open console (f12), and copy paste this script there and press enter.
 	**/
 
 	//  Allows us to stop our code for |ms| milliseconds.
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
- // Convert String into txt and download it. 
+ // Convert String into txt/csv and download it.
 function downloadString(text, fileType, fileName) {
   var blob = new Blob([text], { type: fileType });
 
   var a = document.createElement('a');
   a.download = fileName;
   a.href = URL.createObjectURL(blob);
-  a.dataset.downloadurl = [fileType, a.download, a.href].join(':');
+  a.dataset.downloadurl = [fileType, a.download, a.href].join(',');
   a.style.display = "none";
   document.body.appendChild(a);
   a.click();
@@ -26,7 +26,7 @@ function downloadString(text, fileType, fileName) {
 }
 
 
-function enoughConnections(num) { // if there are less than 50 - it will not work. 
+function enoughConnections(num) { // For future update- I want the program to give alram to users with less than 50 connections that is may not be effective.
 	if (num > 50)
 		return true;
 	else
@@ -46,50 +46,106 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+/*
+// This function belong to the old version and having CSP problem with linkedin. (Doesn't responde well).
+function arrayToCSV(csvContentss) {
+	
+var encodedUri = encodeURI(csvContentss);
+var link = document.createElement("a");
+link.setAttribute("href", encodedUri);
+link.setAttribute("download", "my_data.csv");
+document.body.appendChild(link); // Required for FF
+return link;
+//link.click(); // This will download the data file named "my_data.csv".
+	
+	
+}
+*/
+
+// make the 2d array as a String format to the wanted csv file. 
+function arrayToCSV (twoDiArray) {
+    //  Modified from: http://stackoverflow.com/questions/17836273/
+    //  export-javascript-data-to-csv-file-without-server-interaction
+	// can read more and .csv file format to understand how to manage it. 
+    var csvRows = [];
+    for (var i = 0; i < twoDiArray.length; ++i) {
+        for (var j = 0; j < twoDiArray[i].length; ++j) {
+            twoDiArray[i][j] = '\"' + twoDiArray[i][j] + '\"';  // Handle elements that contain commas
+        }
+        csvRows.push(twoDiArray[i].join(','));
+    }
+
+    var csvString = csvRows.join('\r\n');
+	return csvString;
+}
+
+/*
+// This function belong to the old version. Convert the 2d array into string. NOT IN USE.
+function convertArrayIntoStringBeforeMakeCSV (arr){
+	let csvContentss = "";
+	arr.forEach(function(rowArray){
+	   let row = rowArray.join(",");
+	   csvContentss += row + "\r\n";
+	   
+	 return csvContentss; 
+	   
+});
+	
+}
+	*/
+
 
 //main function:.
 async function addPeople() {
-  count = 0; // count of how many people added. 
+  rowsso = [["#: ","Name: ", "Title: "]];
+  count = 0; // count of how many people added.
   var logString = "people you have been asking to connect with: \n	";
-  while (count < 150){
-	  ul = $('ul.js-mn-discovery-entity-list__pymk')[0]; // a list of persons’s card 
+  while (count < 5){
+	  ul = $('ul.js-mn-discovery-entity-list__pymk')[0]; // a list of persons’s card
 	  ConectionsNodeList = ul.querySelectorAll('li'); // the person’s cards in NodeList object.
-	  var castToArray = Array.prototype.slice.call(ConectionsNodeList); // cast nodeList into array. 
+	  var castToArray = Array.prototype.slice.call(ConectionsNodeList); // cast nodeList into array.
 	  ulRandomIndex = getRandomInt(0,castToArray.length);
 	  randomLi = castToArray[ulRandomIndex]; // random person’s card
-	  while( randomLi != null && count < 150){ // stop after adding x people or when already added all the people in the list. 
+	  while( randomLi != null && count < 5){ // stop after adding x people or when already added all the people in the list.
 		buttonToClick = randomLi.querySelector("button[data-control-name=invite]"); // the button of the first person's card.
 		// make sure that this button contains the text "Connect", because there are also "invite" sometimes.
-		if (buttonToClick.innerText.includes("Connect")){
-		  buttonToClick.click();
-		  count += 1; // If invation has been sent - let the coutner raise.
-		  var nameOfPerson = randomLi.getElementsByClassName('mn-discovery-person-card__name t-14 t-black t-bold')[0].innerHTML; // name of added person 
-		  var occOfPerson = randomLi.getElementsByClassName('mn-discovery-person-card__occupation t-12 t-black--light t-normal')[0].innerHTML;
-		  logString += "number:  " + count + nameOfPerson + occOfPerson;
-		  console.log("I have added " + count + " people so far.");
+		if (buttonToClick.innerText.includes("Connect"))
+		{	
+			buttonToClick.click();
+			count += 1; // If invation has been sent - let the coutner raise.
+			var nameOfPerson = randomLi.getElementsByClassName('mn-discovery-person-card__name t-14 t-black t-bold')[0].innerHTML; // name of added person
+			var occOfPerson = randomLi.getElementsByClassName('mn-discovery-person-card__occupation t-12 t-black--light t-normal')[0].innerHTML;
+			nameOfPerson = new String (nameOfPerson.trim()); // we want to work without whitespace strings in order to create .csv file proprely. 
+			occOfPerson = new String (occOfPerson.trim()); // we want to work without whitespace strings in order to create .csv file proprely. 
+			addToAraay = [count, nameOfPerson, occOfPerson];
+			rowsso.push(addToAraay); // add the 'line' into the the 2d array.
+			  
+			logString += "number:  " + count + "\n" + nameOfPerson + "\n" +occOfPerson; // to present at alret to the user.
+			console.log("I have added " + count + " people so far."); // console information
 		}
-		
-
-		castToArray.splice(ulRandomIndex,1); // remove it. we aleady used it. 
+		castToArray.splice(ulRandomIndex,1); // remove the id card of the linkedin user. we aleady used it.
 		ulRandomIndex = getRandomInt(0,castToArray.length);
 		randomLi = castToArray[ulRandomIndex]; // random person’s card
 
-
-
 		var sleepTimeClick  = getRandomInt(500, 1124);
-		await sleep(sleepTimeClick); // stop this function for 1 second here.
+		await sleep(sleepTimeClick); // stop this function for 0.5 sec to 1.124 secs randomly.
 	  }
+		logString += "\n";
+
+
 	  	 window.scrollTo(0,document.body.scrollTop); // scroll to top, will scroll to bottum after.
-	  for(i = 0; i <5; i++) { // scroll down the page 5 times, 
+	  for(i = 0; i <5; i++) { // scroll down the page 5 times (load more linkedin account objects).
 	 window.scrollTo(0,document.body.scrollHeight);
-	 var sleepTimeScroll  = getRandomInt(1000, 4234);
-	 await sleep(sleepTimeScroll); // let's load some new bitches! (6 secs).
+	 var sleepTimeScroll  = getRandomInt(22, 50);
+	 await sleep(sleepTimeScroll); // let's load some new bitches! .
 	 }
 
 	}
-	alert(logString);
-		window.open('mailto:Nadav.Tarago@Gmail.com?subject=log file&body=' +logString);
-	downloadString(logString,"text", "test.txt");
-  }
-addPeople();
+	prepare2dArrayToCSV= arrayToCSV(rowsso);
+	downloadString(prepare2dArrayToCSV,"csv","logFile.csv");
+	alert(logString); // give an alert about job that have done. 
+		//window.open('mailto:Nadav.Tarago@Gmail.com?subject=log file&body=' +logString); // SEND EMAIL WITH THE ACTIVITIES.
 
+  
+}
+addPeople();
